9b8ee39d23cebf57f86402265c181b1336ba886e
watson <watson-y@outlook.com>
2017-12-01 15:46:58 +0800

    hehe

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('xiaojinjin!');
 });
 
 var server = app.listen(80, function () {
